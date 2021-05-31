4434c2a4b0bfeb2e6b389277976c83e4dc0c3c7b
lucasgualampa <g.brianlucas1993@gmail.com>
2021-05-30 21:07:48 -0300

    init configuration

---
+++ api/src/test/java/com/lucas/em/EmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.em;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
