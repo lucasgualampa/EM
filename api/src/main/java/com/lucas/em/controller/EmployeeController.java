f4a092e20f711e5b250919f8b099e212bc5c09e8
lucasgualampa <g.brianlucas1993@gmail.com>
2021-06-01 21:00:47 -0300

    edited spring cors and updated front

--- api/src/main/java/com/lucas/em/controller/EmployeeController.java
+++ api/src/main/java/com/lucas/em/controller/EmployeeController.java
@@ -12,7 +12,7 @@ import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "/employees", produces = "application/json", method = {RequestMethod.GET, RequestMethod.DELETE, RequestMethod.POST, RequestMethod.PUT})
 public class EmployeeController {
 
